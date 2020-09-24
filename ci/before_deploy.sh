a0d782c5119f430e40a699fe9ef4f60b30a0b72c
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2020-09-24 22:35:10 +0200

    Updated before_deploy.sh

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,10 +18,11 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin hello --target $TARGET --release -- -C lto
+    cross rustc --bin rust-tuya-mqtt --target $TARGET --release
 
     # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/hello $stage/
+    cp target/$TARGET/release/rust-tuya-mqtt $stage/
+    cp config.json $stage/
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
